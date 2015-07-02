2e86856ff58a5655720019b3560ee59581596c18
sandipan.chakrabarti <sandipan.chakrabarti@asurion.com>
2015-07-02 00:33:12 -0700

    added

--- prosperity/book/src/main/java/com/prosperity/filters/SecurityFilter.java
+++ prosperity/book/src/main/java/com/prosperity/filters/SecurityFilter.java
@@ -2,16 +2,13 @@ package com.prosperity.filters;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response.Status;
-import javax.xml.ws.WebServiceException;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import com.data.security.SecurityService;
 import com.security.oauth2.model.Oauth2Model;
-import com.sun.jersey.api.Responses;
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerRequestFilter;
 
